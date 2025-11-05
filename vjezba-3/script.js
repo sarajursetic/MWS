a1a20ba95492db00c98a6eb85747cdc9a1ebb73e
Sara Juršetić <sara.jursetic@gmail.com>
2025-11-05 11:45:57 +0100

    Add files via upload

--- vjezba-3/script.js
+++ vjezba-3/script.js
@@ -1,5 +1,12 @@
 const hambuger = document.querySelector("#hamburger");
+const menu = document.querySelector(".menu ul")
 
 hamburger.onclick = function(){
     this.classList.toggle("open");
+    
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';  
+    } else {
+        menu.style.display = 'flex';  
+    }
 }
